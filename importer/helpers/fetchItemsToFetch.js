5b84e5689cb0556a00ff7595d12e14c8689a342f
Ruben van Eldik <rubenvaneldik@protonmail.com>
2023-01-04 23:23:48 +0100

    fix: Fetch all missing items from the last week when starting the importer

--- importer/helpers/fetchItemsToFetch.js
+++ importer/helpers/fetchItemsToFetch.js
@@ -2,14 +2,14 @@ const queryHackerNews = require('./queryHackerNews')
 const queryHackerDaily = require('./queryHackerDaily')
 
 const query = `
-{
-  stories(order_by: {id: desc}, limit: 1) {
-    id
+  query ($aWeekAgo: timestamptz!) {
+    stories(order_by: {id: asc}, where: {posted_at: {_gte: $aWeekAgo}}) {
+      id
+    }
+    comments(order_by: {id: asc}, where: {posted_at: {_gte: $aWeekAgo}}) {
+      id
+    }
   }
-  comments(order_by: {id: desc}, limit: 1) {
-    id
-  }
-}
 `
 
 /**
@@ -24,17 +24,28 @@ module.exports = async () => {
     throw new Error('Max item on HN is not defined')
   }
 
-  // Query the most recent story and comment
-  const { stories: [lastStory], comments: [lastComment] } = await queryHackerDaily(query)
+  // Calculate the time a week ago
+  const today = new Date()
+  const aWeekAgo = new Date(today.setTime(today.getTime() - (7 * 24 * 60 * 60 * 1000)))
+
+  // Query the IDs of all stored stories and comments in the last week
+  const { stories, comments } = await queryHackerDaily(query, { aWeekAgo })
+
+  // Get a list with all item IDs from last week and get the first item ID of last week
+  const itemIdsFromLastWeek = [...stories.map(({ id }) => id), ...comments.map(({ id }) => id)]
+  const firstItemFromLastWeek = itemIdsFromLastWeek.length ? Math.min(...itemIdsFromLastWeek) : 0
 
   // Get the ID of the first item that should be fetched
-  const maxNumberOfItemsToFetch = 75000
-  const firstItemToFetch = Math.max(lastStory.id ?? 0, lastComment.id ?? 0, maxNewItem - maxNumberOfItemsToFetch) + 1
+  const firstItemToFetch = Math.max(firstItemFromLastWeek, maxNewItem - Math.pow(10, 5)) + 1
 
+  // Check which items from the last week are not stored yet
   const itemsToFetch = []
   for (let i = firstItemToFetch; i <= maxNewItem; i++) {
-    if (!items.includes(i)) itemsToFetch.push(i)
+    if (!itemIdsFromLastWeek.includes(i)) {
+      itemsToFetch.push(i)
+    }
   }
 
+  // Return a list with the item IDs that should be fetched
   return itemsToFetch
 }
